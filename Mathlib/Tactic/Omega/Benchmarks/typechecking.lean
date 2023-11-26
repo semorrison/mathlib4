d7a048bd4e19a88207c227fe868ac8b54d560196
Scott Morrison <scott.morrison@gmail.com>
2023-11-26 16:08:15 +1100

    fast is already slower

--- Mathlib/Tactic/Omega/Benchmarks/typechecking.lean
+++
@@ -1,9 +0,0 @@
-import Mathlib.Tactic.Omega.Frontend
-
-set_option profiler true
-set_option profiler.threshold 1
--- set_option trace.omega true
-
-def foo {x y : Nat} (_ : x / 2 - y / 3 < 1) (_ : 3 * x ≥ 2 * y + 6) : False := by omega
-
--- #print foo
---
+++ Mathlib/Tactic/Omega/Impl2/fast_test.lean
@@ -0,0 +1,15 @@
+import Mathlib.Tactic.Omega.Impl2.fast
+
+def ex1 : Problem := Problem.addInequalities {}
+    [Inequality.of_eq [7, 12, 31] (-17), Inequality.of_eq [3, 5, 24] (-7)]
+
+def ex1_1 : Problem := ex1.addInequalities [Inequality.of_le [1] 1000]
+def ex1_2 : Problem := ex1.addInequalities [Inequality.of_le [0,1] 1000]
+def ex1_3 : Problem := ex1.addInequalities [Inequality.of_le [0,0,1] (-8)]
+def ex1_all : Problem := ex1.addInequalities [Inequality.of_le [1] 1000, Inequality.of_le [0,1] 1000, Inequality.of_le [0,0,1] (-8)]
+
+example : ex1_1.solveEqualities = Problem.addInequalities {} [Inequality.of_le [0,0,-1] 7] := rfl
+example : ex1_2.solveEqualities = Problem.addInequalities {} [Inequality.of_le [0,0,1] 13] := rfl
+example : ex1_3.solveEqualities = Problem.addInequalities {} [Inequality.of_le [0,0,1] (-8)] := rfl
+
+example : ex1_all.solveEqualities.possible = false := rfl
